323b9e76c394de81b0d14fbbdea4d9acded117b7
Legandjl <38377030+Legandjl@users.noreply.github.com>
2021-10-14 17:16:28 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>hello world</p>
+      </div>
+    );
+  }
+}
+
+export default App;
